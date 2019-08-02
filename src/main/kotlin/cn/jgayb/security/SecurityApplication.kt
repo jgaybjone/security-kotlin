0bebdc6987c906815905034ab49bb1afad36c029
jungang <82099359@qq.com>
2019-08-02 19:45:09 +0800

    chore：初始化项目

---
+++ src/main/kotlin/cn/jgayb/security/SecurityApplication.kt
@@ -0,0 +1,11 @@
+package cn.jgayb.security
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecurityApplication
+
+fun main(args: Array<String>) {
+    runApplication<SecurityApplication>(*args)
+}
